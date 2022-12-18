3effd610c9ae08f37f975e4d9a53ec3ea0fe7eb7
indian9361072610 <111270826+indian9361072610@users.noreply.github.com>
2022-12-18 11:10:14 -0800

    Update build_files.sh

--- build_files.sh
+++ build_files.sh
@@ -1,6 +1,10 @@
 # build_files.sh
-pip install -r requirements.txt
+echo "build.."
+python3.9 -m pip install -r requirements.txt
+echo "make migration ...."
 python3.9 manage.py makemigrations --noinput
 python3.9 manage.py migrate --noinput
+
+echo "collect staic  .."
 python3.9 manage.py collectstatic --noinput --clear
 
