d56ca1c52dc17836814b54fad8aceb1a5e4df3f6
indian9361072610 <111270826+indian9361072610@users.noreply.github.com>
2022-12-19 01:26:06 -0800

    Update settings.py

--- park/settings.py
+++ park/settings.py
@@ -146,8 +146,8 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 #static work
 
 
-STATICFILES_DIRS = os.path.join(BASE_DIR,'static'),
-STATIC_ROOT = os.path.join(BASE_DIR/ 'staticfiles_build'/ 'static')
+STATICFILES_DIRS = os.path.join(BASE_DIR,'static')
+STATIC_ROOT = os.path.join(BASE_DIR,'staticfiles_build','static')
 
 
 
